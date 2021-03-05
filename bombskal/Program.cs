8293a886d20c5e7066bb817ab7c2b33669d622e5
Torsus <crosis767@gmail.com>
2021-03-05 13:23:54 +0100

    c55

---
+++ bombskal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace bombskal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //test2////////////////////
+            Process.Start("bomb.exe");
+        }
+    }
+}
